d32f772e5274e8490ade20e88c6bfa10b8ecfee5
John Preston <johnprestonmail@gmail.com>
2019-11-08 14:08:03 +0300

    Divide codegen cmake scripts.

---
+++ cmake/generate_emoji.cmake
@@ -0,0 +1,23 @@
+function(generate_emoji target_name suggestions_json)
+    set(gen_dst ${CMAKE_CURRENT_BINARY_DIR}/gen)
+    set(gen_src ${CMAKE_CURRENT_SOURCE_DIR}/${suggestions_json})
+    set(generated_files
+        ${gen_dst}/emoji.cpp
+        ${gen_dst}/emoji.h
+        ${gen_dst}/emoji_suggestions_data.cpp
+        ${gen_dst}/emoji_suggestions_data.h
+    )
+    add_custom_command(
+    OUTPUT
+        ${generated_files}
+    COMMAND
+        codegen_emoji
+        -o${gen_dst}
+        ${gen_src}
+    COMMENT "Generating emoji (${target_name})"
+    DEPENDS
+        codegen_emoji
+        ${gen_src}
+    )
+    generate_target(${target_name} emoji "${generated_files}" ${gen_dst})
+endfunction()
---
+++ cmake/generate_palette.cmake
@@ -0,0 +1,25 @@
+function(generate_palette target_name palette_file)
+    set(gen_dst ${CMAKE_CURRENT_BINARY_DIR}/gen)
+    set(gen_src ${CMAKE_CURRENT_SOURCE_DIR}/${palette_file})
+    set(generated_files
+        ${gen_dst}/styles/palette.cpp
+        ${gen_dst}/styles/palette.h
+    )
+    add_custom_command(
+    OUTPUT
+        ${generated_files}
+    COMMAND
+        codegen_style
+        -I${gen_dst}
+        -o${gen_dst}/styles
+        -w${CMAKE_CURRENT_SOURCE_DIR}
+        ${gen_src}
+    COMMENT "Generating palette (${target_name})"
+    DEPENDS
+        codegen_style
+        ${gen_src}
+    MAIN_DEPENDENCY
+        ${gen_src}
+    )
+    generate_target(${target_name} palette "${generated_files}" ${gen_dst})
+endfunction()
