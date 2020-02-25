desktop-app/lib_ui

commit 721d143c89833dc15ae76810089180bf562ac707

Fix rounding of sparse QImage-s.

ui/image/image_prepare.cpp
@@ -469,10 +469,10 @@ void prepareRound(
 	Assert(image.bytesPerLine() == (imageIntsPerLine << 2));
 
 	auto ints = reinterpret_cast<uint32*>(image.bits());
-	auto intsTopLeft = ints + target.x() + target.y() * imageWidth;
-	auto intsTopRight = ints + target.x() + target.width() - cornerWidth + target.y() * imageWidth;
-	auto intsBottomLeft = ints + target.x() + (target.y() + target.height() - cornerHeight) * imageWidth;
-	auto intsBottomRight = ints + target.x() + target.width() - cornerWidth + (target.y() + target.height() - cornerHeight) * imageWidth;
+	auto intsTopLeft = ints + target.x() + target.y() * imageIntsPerLine;
+	auto intsTopRight = ints + target.x() + target.width() - cornerWidth + target.y() * imageIntsPerLine;
+	auto intsBottomLeft = ints + target.x() + (target.y() + target.height() - cornerHeight) * imageIntsPerLine;
+	auto intsBottomRight = ints + target.x() + target.width() - cornerWidth + (target.y() + target.height() - cornerHeight) * imageIntsPerLine;
 	auto maskCorner = [&](uint32 *imageInts, const QImage &mask) {
 		auto maskWidth = mask.width();
 		auto maskHeight = mask.height();

commit ccc12ce3da8f9ac30f30228c225585f732f74d5f

Fix crash in rounding of shared images.
Regression was introduced in 721d143c89.

ui/image/image_prepare.cpp
@@ -463,12 +463,17 @@ void prepareRound(
 	if (imageWidth < 2 * cornerWidth || imageHeight < 2 * cornerHeight) {
 		return;
 	}
+
+	// We need to detach image first (if it is shared), before we
+	// count some offsets using QImage::bytesPerLine etc, because
+	// bytesPerLine may change on detach, this leads to crashes:
+	// Real image bytesPerLine is smaller than the one we use for offsets.
+	auto ints = reinterpret_cast<uint32*>(image.bits());
+
 	constexpr auto imageIntsPerPixel = 1;
 	auto imageIntsPerLine = (image.bytesPerLine() >> 2);
 	Assert(image.depth() == static_cast<int>((imageIntsPerPixel * sizeof(uint32)) << 3));
 	Assert(image.bytesPerLine() == (imageIntsPerLine << 2));
-
-	auto ints = reinterpret_cast<uint32*>(image.bits());
 	auto intsTopLeft = ints + target.x() + target.y() * imageIntsPerLine;
 	auto intsTopRight = ints + target.x() + target.width() - cornerWidth + target.y() * imageIntsPerLine;
 	auto intsBottomLeft = ints + target.x() + (target.y() + target.height() - cornerHeight) * imageIntsPerLine;