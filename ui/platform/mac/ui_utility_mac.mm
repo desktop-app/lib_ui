83fada46757d456e8ad160ab65172e6e1f370b67
John Preston <johnprestonmail@gmail.com>
2020-05-05 13:51:38 +0400

    Fix separate panels transparent background.
    Regression was introduced with migration to Qt 5.12.8.

--- ui/platform/mac/ui_utility_mac.mm
+++ ui/platform/mac/ui_utility_mac.mm
@@ -41,6 +41,7 @@ void InitOnTopPanel(not_null<QWidget*> panel) {
 	Assert([platformWindow isKindOfClass:[NSPanel class]]);
 
 	auto platformPanel = static_cast<NSPanel*>(platformWindow);
+	[platformPanel setBackgroundColor:[NSColor clearColor]];
 	[platformPanel setLevel:NSModalPanelWindowLevel];
 	[platformPanel setCollectionBehavior:NSWindowCollectionBehaviorCanJoinAllSpaces|NSWindowCollectionBehaviorStationary|NSWindowCollectionBehaviorFullScreenAuxiliary|NSWindowCollectionBehaviorIgnoresCycle];
 	[platformPanel setHidesOnDeactivate:NO];
