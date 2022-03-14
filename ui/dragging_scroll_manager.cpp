6e6ba5a14947242ae0de3296fd650a3b3338728e
23rd <23rd@vivaldi.net>
2022-03-14 17:19:59 +0300

    Moved Ui::SelectScrollManager to lib_ui.

---
+++ ui/dragging_scroll_manager.cpp
@@ -0,0 +1,50 @@
+/*
+This file is part of Telegram Desktop,
+the official desktop application for the Telegram messaging service.
+
+For license and copyright information please follow this link:
+https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
+*/
+#include "ui/dragging_scroll_manager.h"
+
+#include "ui/widgets/scroll_area.h"
+
+namespace Ui {
+
+DraggingScrollManager::DraggingScrollManager()
+: _timer([=] { scrollByTimer(); }) {
+}
+
+void DraggingScrollManager::scrollByTimer() {
+	const auto d = (_delta > 0)
+		? std::min(_delta * 3 / 20 + 1, kMaxScrollSpeed)
+		: std::max(_delta * 3 / 20 - 1, -kMaxScrollSpeed);
+	_scrolls.fire_copy(d);
+}
+
+void DraggingScrollManager::checkDeltaScroll(
+		const QPoint &point,
+		int top,
+		int bottom) {
+	const auto diff = point.y() - top;
+	_delta = (diff < 0)
+		? diff
+		: (point.y() >= bottom)
+		? (point.y() - bottom + 1)
+		: 0;
+	if (_delta) {
+		_timer.callEach(15);
+	} else {
+		_timer.cancel();
+	}
+}
+
+void DraggingScrollManager::cancel() {
+	_timer.cancel();
+}
+
+rpl::producer<int> DraggingScrollManager::scrolls() {
+	return _scrolls.events();
+}
+
+} // namespace Ui
