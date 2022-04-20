4093d6605a901a9ae8bdd8fe877dd9cdb05ac9c6
John Preston <johnprestonmail@gmail.com>
2022-04-20 11:35:11 +0400

    Don't attach menu callback to QAction lifetime.

--- ui/widgets/menu/menu_common.cpp
+++ ui/widgets/menu/menu_common.cpp
@@ -6,6 +6,9 @@
 //
 #include "ui/widgets/menu/menu_common.h"
 
+#include "ui/ui_utility.h"
+#include "base/invoke_queued.h"
+
 #include <QAction>
 
 namespace Ui::Menu {
@@ -15,12 +18,11 @@ not_null<QAction*> CreateAction(
 		const QString &text,
 		Fn<void()> &&callback) {
 	const auto action = new QAction(text, parent);
-	parent->connect(
-		action,
-		&QAction::triggered,
-		action,
-		std::move(callback),
-		Qt::QueuedConnection);
+	const auto guard = MakeWeak(parent);
+	auto triggered = [guard, callback = std::move(callback)]() mutable {
+		InvokeQueued(guard, std::move(callback));
+	};
+	parent->connect(action, &QAction::triggered, std::move(triggered));
 	return action;
 }
 
