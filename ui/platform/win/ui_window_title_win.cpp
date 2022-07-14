f0070960716ad993243dc49901f7db631d38981c
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-07-14 12:54:15 +0400

    Adapt for Qt 6.4 device pixel ratio changes
    QWidget::devicePixelRatioF represents backing store ratio and it's no more equal to native surface ratio

--- ui/platform/win/ui_window_title_win.cpp
+++ ui/platform/win/ui_window_title_win.cpp
@@ -205,7 +205,8 @@ void TitleWidget::refreshAdditionalPaddings(
 			return -1;
 		}
 		const auto pixels = (factor + 50) / 100;
-		return int(base::SafeRound(pixels / window()->devicePixelRatioF()));
+		return int(base::SafeRound(
+			pixels / window()->windowHandle()->devicePixelRatio()));
 	}();
 	if (padding < 0) {
 		return;
