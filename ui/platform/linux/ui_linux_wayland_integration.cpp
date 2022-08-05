4689c896e201cc2863d629eac58306a0b561a71c
Ilya Fedin <fedin-ilja2010@ya.ru>
2022-08-05 14:45:42 +0400

    Fix packaged build

--- ui/platform/linux/ui_linux_wayland_integration.cpp
+++ ui/platform/linux/ui_linux_wayland_integration.cpp
@@ -12,6 +12,11 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include "waylandshells/xdg_shell.h"
 #include "qwayland-xdg-shell.h"
 
+// must be before QtPlugin include
+#ifndef QT_STATICPLUGIN
+#define QT_STATICPLUGIN
+#endif // !QT_STATICPLUGIN
+
 #include <QtCore/QtPlugin>
 #include <QtGui/QGuiApplication>
 #include <QtGui/QWindow>
@@ -23,10 +28,6 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #define signals Q_SIGNALS
 #define slots Q_SLOTS
 #endif // QT_NO_KEYWORDS
-
-#ifndef QT_STATICPLUGIN
-#define QT_STATICPLUGIN
-#endif // !QT_STATICPLUGIN
 
 #include <private/qwaylanddisplay_p.h>
 #include <private/qwaylandwindow_p.h>
@@ -70,8 +71,9 @@ public:
 
 	QRect geometry() const override {
 		if (!_settingGeometry) {
-			if (const auto widgetWindow = qobject_cast<const QWidgetWindow*>(
-				window())) {
+			if (window()->inherits("QWidgetWindow")) {
+				const auto widgetWindow = static_cast<QWidgetWindow*>(
+					window());
 				return widgetWindow->widget()->geometry();
 			}
 		}
