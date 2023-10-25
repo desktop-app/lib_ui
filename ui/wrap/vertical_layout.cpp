b05f7eb915a86f67249904061d70f293066de618
John Preston <johnprestonmail@gmail.com>
2023-10-25 21:23:30 +0400

    Resize widgets after adding to VerticalLayout.

--- ui/wrap/vertical_layout.cpp
+++ ui/wrap/vertical_layout.cpp
@@ -126,6 +126,10 @@ RpWidget *VerticalLayout::insertChild(
 		_rows.insert(
 			begin(_rows) + atPosition,
 			{ std::move(child), margin });
+		auto availRowWidth = widthNoMargins()
+			- margin.left()
+			- margin.right();
+		weak->resizeToNaturalWidth(availRowWidth);
 		weak->heightValue(
 		) | rpl::start_with_next_done([=] {
 			if (!_inResize) {
