48dd6b9d76396457773d6e85f4f63158edb78be8
Reza Bakhshi Laktasaraei <74649066+rezabakhshilaktasaraei@users.noreply.github.com>
2025-10-18 10:17:44 +0330

    refactor: replace deprecated QAccessible::PushButton with Button

--- ui/widgets/buttons.cpp
+++ ui/widgets/buttons.cpp
@@ -235,7 +235,7 @@ FlatButton::FlatButton(
 		_width = _st.width;
 	}
 	resize(_width, _st.height);
-	setAccessibleRole(QAccessible::Role::PushButton);
+	setAccessibleRole(QAccessible::Role::Button);
 	setAccessibleName(text);
 }
 
@@ -307,7 +307,7 @@ RoundButton::RoundButton(
 , _st(st)
 , _roundRect(st.radius ? st.radius : st::buttonRadius, _st.textBg)
 , _roundRectOver(st.radius ? st.radius : st::buttonRadius, _st.textBgOver) {
-	setAccessibleRole(QAccessible::Role::PushButton);
+	setAccessibleRole(QAccessible::Role::Button);
 	_textFull.value(
 	) | rpl::start_with_next([=](const TextWithEntities &text) {
 		resizeToText(text);
@@ -559,7 +559,7 @@ RoundButton::~RoundButton() = default;
 IconButton::IconButton(QWidget *parent, const style::IconButton &st) : RippleButton(parent, st.ripple)
 , _st(st) {
 	resize(_st.width, _st.height);
-	setAccessibleRole(QAccessible::Role::PushButton);
+	setAccessibleRole(QAccessible::Role::Button);
 }
 
 const style::IconButton &IconButton::st() const {
@@ -657,7 +657,7 @@ CrossButton::CrossButton(QWidget *parent, const style::CrossButton &st) : Ripple
 	resize(_st.width, _st.height);
 	setCursor(style::cur_pointer);
 	setVisible(false);
-	setAccessibleRole(QAccessible::Role::PushButton);
+	setAccessibleRole(QAccessible::Role::Button);
 }
 
 bool CrossButton::loadingCallback(crl::time now) {
@@ -803,7 +803,7 @@ SettingsButton::SettingsButton(
 , _st(st)
 , _padding(_st.padding)
 , _context(context) {
-	setAccessibleRole(QAccessible::Role::PushButton);
+	setAccessibleRole(QAccessible::Role::Button);
 	std::move(
 		text
 	) | rpl::start_with_next([this](TextWithEntities &&value) {
@@ -818,7 +818,7 @@ SettingsButton::SettingsButton(
 : RippleButton(parent, st.ripple)
 , _st(st)
 , _padding(_st.padding) {
-	setAccessibleRole(QAccessible::Role::PushButton);
+	setAccessibleRole(QAccessible::Role::Button);
 }
 
 SettingsButton::~SettingsButton() = default;
