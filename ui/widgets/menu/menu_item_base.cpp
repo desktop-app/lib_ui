desktop-app/lib_ui

commit 478c5133a4ee189ec9724cdf928703d9321a63a8

Added right press-move-release gesture to menu on Unix systems.

ui/widgets/menu/menu_item_base.cpp
@@ -116,4 +116,14 @@ void ItemBase::enableMouseSelecting(not_null<RpWidget*> widget) {
 	}, lifetime());
 }
 
+#ifdef Q_OS_UNIX
+void ItemBase::mouseReleaseEvent(QMouseEvent *e) {
+	RippleButton::mouseReleaseEvent(e);
+	if (isEnabled() && e->button() == Qt::RightButton) {
+		setClicked(TriggeredSource::Mouse);
+		return;
+	}
+}
+#endif // Q_OS_UNIX
+
 } // namespace Ui::Menu

ui/widgets/menu/menu_item_base.h
@@ -52,6 +52,10 @@ protected:
 
 	virtual int contentHeight() const = 0;
 
+#ifdef Q_OS_UNIX
+	void mouseReleaseEvent(QMouseEvent *e) override;
+#endif // Q_OS_UNIX
+
 private:
 	int _index = -1;
 

commit aedb11a30aed40d822ea680c70c7b09a61773289

Fixed crash from context menu.
Regression was introduced in 478c5133a4.

ui/widgets/menu/menu_item_base.cpp
@@ -118,11 +118,11 @@ void ItemBase::enableMouseSelecting(not_null<RpWidget*> widget) {
 
 #ifdef Q_OS_UNIX
 void ItemBase::mouseReleaseEvent(QMouseEvent *e) {
-	RippleButton::mouseReleaseEvent(e);
 	if (isEnabled() && e->button() == Qt::RightButton) {
 		setClicked(TriggeredSource::Mouse);
 		return;
 	}
+	RippleButton::mouseReleaseEvent(e);
 }
 #endif // Q_OS_UNIX
 