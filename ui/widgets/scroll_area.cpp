1ffc08e4aafc21f8b48c99db47d5621cef212ab1
Polina Vishneva <poli@0iq.dev>
2026-07-22 23:39:01 +0200

    Use QWheelEvent timestamps instead of crl::now().

--- ui/widgets/scroll_area.cpp
+++ ui/widgets/scroll_area.cpp
@@ -740,9 +740,16 @@ bool ScrollArea::viewportEvent(QEvent *e) {
 							unmultiplied.y() * std::max(height(), 120) / 120.)
 						: unmultiplied;
 				}
-				_scroller->handleInput(wasNull
-					? KineticScroller::InputPress
-					: KineticScroller::InputMove, _wheelPos, crl::now());
+				// Estimate the velocity by the events' own timestamps:
+				// some transports (e.g. bluetooth) may sometimes deliver
+				// several events at once, and their identical processing times
+				// would break the estimation.
+				_scroller->handleInput(
+					(wasNull
+						? KineticScroller::InputPress
+						: KineticScroller::InputMove),
+					_wheelPos,
+					crl::time(ev->timestamp()));
 			} return true;
 			case Qt::ScrollEnd:
 			case Qt::ScrollMomentum: {
@@ -750,7 +757,7 @@ bool ScrollArea::viewportEvent(QEvent *e) {
 					_scroller->handleInput(
 						KineticScroller::InputRelease,
 						_wheelPos,
-						crl::now());
+						crl::time(ev->timestamp()));
 					_wheelPos = {};
 				}
 			} return true;
