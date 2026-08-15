desktop-app/lib_ui

commit c9a97514723d945c71dc73918f4fa8aaf002723f

Added utilities to mid and filter TextWithEntities.

ui/text/text_utilities.cpp
@@ -74,5 +74,48 @@ TextWithEntities RichLangValue(const QString &text) {
 	return result;
 }
 
+TextWithEntities Mid(const TextWithEntities &text, int position, int n) {
+	if (n == -1) {
+		n = int(text.text.size()) - position;
+	}
+	const auto midEnd = (position + n);
+	auto entities = ranges::views::all(
+		text.entities
+	) | ranges::views::filter([&](const EntityInText &entity) {
+		// Intersects of ranges.
+		const auto l1 = entity.offset();
+		const auto r1 = entity.offset() + entity.length() - 1;
+		const auto l2 = position;
+		const auto r2 = midEnd - 1;
+		return !(l1 > r2 || l2 > r1);
+	}) | ranges::views::transform([&](const EntityInText &entity) {
+		if ((entity.offset() == position) && (entity.length() == n)) {
+			return entity;
+		}
+		const auto start = std::max(entity.offset(), position);
+		const auto end = std::min(entity.offset() + entity.length(), midEnd);
+		return EntityInText(
+			entity.type(),
+			start - position,
+			end - start,
+			entity.data());
+	}) | ranges::to<EntitiesInText>();
+	return {
+		.text = text.text.mid(position, n),
+		.entities = std::move(entities),
+	};
+}
+
+TextWithEntities Filtered(
+		const TextWithEntities &text,
+		const std::vector<EntityType> &types) {
+	auto result = ranges::views::all(
+		text.entities
+	) | ranges::views::filter([&](const EntityInText &entity) {
+		return ranges::contains(types, entity.type());
+	}) | ranges::to<EntitiesInText>();
+	return { .text = text.text, .entities = std::move(result) };
+}
+
 } // namespace Text
 } // namespace Ui

ui/text/text_utilities.h
@@ -65,5 +65,13 @@ inline constexpr auto Upper = details::ToUpperType{};
 	return rpl::map(WithEntities);
 }
 
+[[nodiscard]] TextWithEntities Mid(
+	const TextWithEntities &text,
+	int position,
+	int n = -1);
+[[nodiscard]] TextWithEntities Filtered(
+	const TextWithEntities &result,
+	const std::vector<EntityType> &types);
+
 } // namespace Text
 } // namespace Ui

commit 137ff1dcf443c7dfb7f95f8cc4a32d1fb75e81d6

Fixed entity offset in Mid for entity covering whole range.
Regression was introduced in c9a9751472.

ui/text/text_utilities.cpp
@@ -251,9 +251,6 @@ TextWithEntities Mid(const TextWithEntities &text, int position, int n) {
 		const auto r2 = midEnd - 1;
 		return !(l1 > r2 || l2 > r1);
 	}) | ranges::views::transform([&](const EntityInText &entity) {
-		if ((entity.offset() == position) && (entity.length() == n)) {
-			return entity;
-		}
 		const auto start = std::max(entity.offset(), position);
 		const auto end = std::min(entity.offset() + entity.length(), midEnd);
 		return EntityInText(