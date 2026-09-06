f14c2fd63e586307372f8fccf01f785a988148b8
23rd <23rd@vivaldi.net>
2026-09-06 10:16:14 +0300

    Fixed submenu item clicks with Qt 6.11 on Windows.

--- ui/widgets/popup_menu.cpp
+++ ui/widgets/popup_menu.cpp
@@ -1155,7 +1155,9 @@ void PopupMenu::showPrepared(TriggeredSource source) {
 	}
 	Platform::ShowOverAll(this);
 	raise();
-	activateWindow();
+	if (!_parent) {
+		activateWindow();
+	}
 	if (Ui::ScreenReaderModeActive()) {
 		_menu->setShowSource(TriggeredSource::Keyboard);
 	} else {
