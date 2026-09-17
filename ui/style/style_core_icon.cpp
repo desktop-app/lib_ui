ae492d4015ce35daf697053776c79786ea7d9282
John Preston <johnprestonmail@gmail.com>
2026-09-17 08:30:03 +0400

    Use std::pair in iconPixmaps.

--- ui/style/style_core_icon.cpp
+++ ui/style/style_core_icon.cpp
@@ -29,7 +29,7 @@ namespace {
 base::flat_map<const IconMask*, QImage> IconMasks;
 QMutex IconMasksMutex;
 
-base::flat_map<QPair<const IconMask*, uint32>, QPixmap> iconPixmaps;
+base::flat_map<std::pair<const IconMask*, uint32>, QPixmap> iconPixmaps;
 base::flat_set<IconData*> iconData;
 
 [[nodiscard]] QImage CreateIconMask(
